6376afefbef900e046fc1b45db80951ee8514a98
Evgenii <170091238+evgeguli@users.noreply.github.com>
2024-05-21 12:29:14 +0200

    Implement saving tasks in localStorage
    Every time when user add/delete or change task all tasks save
    in localStorage.

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,49 +16,60 @@ const addTaskEl = (task) => {
     const taskEl = taskTemplateClone.querySelector('.task');
 
     taskEl.setAttribute('data-task-id', task.id);
+    if (task.finished) taskEl.classList.add('completed');
     const taskNameEl = taskEl.querySelector('.task-name');
     taskNameEl.textContent = task.name;
 
     // Add event listeners
     const deleteButton = taskEl.querySelector('.delete-button');
-    deleteButton.addEventListener('click', removeTask);
+    deleteButton.addEventListener('click', removeTaskHandler);
     const checkboxButton = taskEl.querySelector('.checkbox-button');
-    checkboxButton.addEventListener('click', toggleTask);
+    checkboxButton.addEventListener('click', toggleTaskHandler);
 
     elements.todoUl.append(taskEl);
 };
 
-const addTask = (evt) => {
+const reloadTaskList = () => {
+    for (const task of tasks.values()) {
+        addTaskEl(task);
+    }
+}
+
+// EVENT HANDLERS
+const addTaskHandler = (evt) => {
     const name = elements.nameInput.value;
     if (name) {
-        const task = new Task(name);
-        tasks.set(task.id, task);
+        let task = createTask(name)
         addTaskEl(task);
     } else {
         alert('Der Aufgabenname darf nicht leer sein.');
     }
 }
 
-const removeTask = (evt) => {
+const removeTaskHandler = (evt) => {
     const taskEl = evt.currentTarget.closest('.task');
-    const task_id = Number(taskEl.getAttribute('data-task-id'));
-    
-    tasks.delete(task_id);
+    const task_id = taskEl.getAttribute('data-task-id');
+
+    deleteTask(task_id);
     taskEl.remove();
 }
 
-const toggleTask = (evt) => {
+const toggleTaskHandler = (evt) => {
     const taskEl = evt.currentTarget.closest('.task');
     taskEl.classList.toggle('completed')
+
+    const task_id = taskEl.getAttribute('data-task-id');
+    toggleTask(task_id);
 }
 
 const appendEventlisteners = () => {
-    elements.addButton.addEventListener('click', addTask);
+    elements.addButton.addEventListener('click', addTaskHandler);
 }
 
 const init = () => {
     domMapping();
     appendEventlisteners();
+    reloadTaskList();
 }
 
 // INIT
--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,10 +1,36 @@
 'use strict';
 
-const tasks = new Map();
+// load tasks from localStorage
+let tasks_json = localStorage.getItem('tasks');
+const tasks = new Map(JSON.parse(tasks_json));
 
 class Task {
     constructor(name) {
         this.id = self.crypto.randomUUID();
         this.name = name;
+        this.finished = false;
     }
+}
+
+const createTask = (name) => {
+    let task = new Task(name);
+    tasks.set(task.id, task);
+    storeTasks();
+    return task;
+}
+
+const deleteTask = (task_id) => {
+    tasks.delete(task_id);
+    storeTasks();
+}
+
+const toggleTask = (task_id) => {
+    let task = tasks.get(task_id);
+    task.finished = !task.finished;
+    storeTasks();
+}
+
+const storeTasks = () => {
+    let tasks_json = JSON.stringify([...tasks]);
+    localStorage.setItem('tasks', tasks_json);
 }
